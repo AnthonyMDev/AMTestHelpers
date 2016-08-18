ea2684a7f684b6eab75f478d5880e6cab8ca002e
Anthony Miller <anthony@app-order.com>
2016-08-18 13:28:42 -0700

    Fixed bug with UIResponder Added swizzling for 'presentedViewController' and 'presentingViewController' on 'UIViewController'

--- Pod/Classes/UIResponder+TestHelpers.swift
+++ Pod/Classes/UIResponder+TestHelpers.swift
@@ -9,7 +9,7 @@ import UIKit
 
 import JRSwizzle
 
-extension UITextField {
+extension UIResponder {
   public override class func initialize() {
     struct Static {
       static var becomeFirstResponderToken: dispatch_once_t = 0
@@ -18,25 +18,26 @@ extension UITextField {
     }
     
     dispatch_once(&Static.becomeFirstResponderToken) {
-      let originalSelector = Selector("becomeFirstResponder")
-      let swizzledSelector = Selector("AM_testBecomeFirstResponder")
-      do { try UITextField.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
+        let originalSelector = #selector(UIResponder.becomeFirstResponder)
+        let swizzledSelector = #selector(UIResponder.AM_testBecomeFirstResponder)
+      do { try UIResponder.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
         debugPrint(error)
       }
     }
     
     dispatch_once(&Static.resignFirstResponderToken) {
-      let originalSelector = Selector("resignFirstResponder")
-      let swizzledSelector = Selector("AM_testResignFirstResponder")
-      do { try UITextField.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
+        let originalSelector = #selector(UIResponder.resignFirstResponder)
+        let swizzledSelector = #selector(UIResponder.AM_testResignFirstResponder)
+      do { try UIResponder.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
         debugPrint(error)
       }
     }
     
     dispatch_once(&Static.isFirstResponderToken) {
-      let originalSelector = Selector("isFirstResponder")
-      let swizzledSelector = Selector("AM_testIsFirstResponder")
-      do { try UITextField.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
+        let originalSelector = #selector(UIResponder.isFirstResponder)
+        let swizzledSelector = #selector(UIResponder.AM_testIsFirstResponder)
+        
+      do { try UIResponder.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
         debugPrint(error)
       }
     }
@@ -44,15 +45,15 @@ extension UITextField {
   }
   
   func AM_testBecomeFirstResponder() -> Bool {
-    self.AM_testBecomeFirstResponder()
     AM_testIsFirstResponderValue = true
+    self.AM_testBecomeFirstResponder()
     
     return AM_testIsFirstResponderValue
   }
-  
+    
   func AM_testResignFirstResponder() -> Bool {
-    self.AM_testResignFirstResponder()
     AM_testIsFirstResponderValue = false
+    self.AM_testResignFirstResponder()
     
     return AM_testIsFirstResponderValue
   }
@@ -65,7 +66,7 @@ extension UITextField {
     static var TestIsFirstResponderValue = "AM_testIsFirstResponderValue"
   }
   
-  public var AM_testIsFirstResponderValue: Bool {
+  private var AM_testIsFirstResponderValue: Bool {
     get {
       if let associatedObject = objc_getAssociatedObject(self, &AssociatedKeys.TestIsFirstResponderValue) as? Bool {
         return associatedObject
@@ -83,76 +84,131 @@ extension UITextField {
   
 }
 
-extension UITextView {
+extension UIView {
+    public override class func initialize() {
+        struct Static {
+            static var windowIfNilToken: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.windowIfNilToken) {
+            let originalSelector = Selector("window")
+            let swizzledSelector = #selector(UIView.AM_windowIfNil)
+            do { try UIView.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
+                debugPrint(error)
+            }
+        }
+    }
+    
+    func AM_windowIfNil() -> UIWindow? {
+        if let window = AM_windowIfNil() {
+            return window
+        }
+        if shouldHaveMockWindow {
+            return UIWindow()
+        }
+        
+        return nil
+    }
+    
+    private struct AssociatedKeys {
+        static var ShouldHaveMockWindow = "AM_testShouldHaveMockWindow"
+    }
+    
+    private var shouldHaveMockWindow: Bool {
+        get {
+            if let associatedObject = objc_getAssociatedObject(self, &AssociatedKeys.ShouldHaveMockWindow) as? Bool {
+                return associatedObject
+                
+            }
+            return false
+        }
+        set {
+            objc_setAssociatedObject(self,
+                                     &AssociatedKeys.ShouldHaveMockWindow,
+                                     newValue,
+                                     objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN)
+        }
+    }
+    
+}
+
+extension UITextField {
   public override class func initialize() {
     struct Static {
       static var becomeFirstResponderToken: dispatch_once_t = 0
       static var resignFirstResponderToken: dispatch_once_t = 0
-      static var isFirstResponderToken: dispatch_once_t = 0
     }
-    
+
     dispatch_once(&Static.becomeFirstResponderToken) {
-      let originalSelector = Selector("becomeFirstResponder")
-      let swizzledSelector = Selector("AM_testBecomeFirstResponder")
-      do { try UITextView.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
+      let originalSelector = #selector(UITextField.becomeFirstResponder)
+      let swizzledSelector = #selector(UITextField.AM_testBypassBecomeFirstResponder)
+      do { try UITextField.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
         debugPrint(error)
       }
     }
-    
+
     dispatch_once(&Static.resignFirstResponderToken) {
-      let originalSelector = Selector("resignFirstResponder")
-      let swizzledSelector = Selector("AM_testResignFirstResponder")
-      do { try UITextView.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
+      let originalSelector = #selector(UITextField.resignFirstResponder)
+      let swizzledSelector = #selector(UITextField.AM_testBypassResignFirstResponder)
+      do { try UITextField.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
         debugPrint(error)
       }
     }
+
+  }
+
+  func AM_testBypassBecomeFirstResponder() -> Bool {
+    shouldHaveMockWindow = true
+    defer { shouldHaveMockWindow = false }
+    return self.AM_testBypassBecomeFirstResponder()
     
-    dispatch_once(&Static.isFirstResponderToken) {
-      let originalSelector = Selector("isFirstResponder")
-      let swizzledSelector = Selector("AM_testIsFirstResponder")
-      do { try UITextView.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
-        debugPrint(error)
-      }
-    }
-    
   }
-  
-  func AM_testBecomeFirstResponder() -> Bool {
-    self.AM_testBecomeFirstResponder()
-    AM_testIsFirstResponderValue = true
-    
-    return AM_testIsFirstResponderValue
+
+  func AM_testBypassResignFirstResponder() -> Bool {
+    shouldHaveMockWindow = true
+    defer { shouldHaveMockWindow = false }
+    return self.AM_testBypassResignFirstResponder()
   }
-  
-  func AM_testResignFirstResponder() -> Bool {
-    self.AM_testResignFirstResponder()
-    AM_testIsFirstResponderValue = false
-    
-    return AM_testIsFirstResponderValue
-  }
-  
-  func AM_testIsFirstResponder() -> Bool {
-    return AM_testIsFirstResponderValue
-  }
-  
-  private struct AssociatedKeys {
-    static var TestIsFirstResponderValue = "AM_testIsFirstResponderValue"
-  }
-  
-  public var AM_testIsFirstResponderValue: Bool {
-    get {
-      if let associatedObject = objc_getAssociatedObject(self, &AssociatedKeys.TestIsFirstResponderValue) as? Bool {
-        return associatedObject
-        
-      }
-      return false
-    }
-    set {
-      objc_setAssociatedObject(self,
-        &AssociatedKeys.TestIsFirstResponderValue,
-        newValue,
-        objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN)
-    }
-  }
-  
+
 }
+
+extension UITextView {
+    public override class func initialize() {
+        struct Static {
+            static var becomeFirstResponderToken: dispatch_once_t = 0
+            static var resignFirstResponderToken: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.becomeFirstResponderToken) {
+            let originalSelector = #selector(UITextView.becomeFirstResponder)
+            let swizzledSelector = #selector(UITextView.AM_testBypassBecomeFirstResponder)
+            do { try UITextView.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
+                debugPrint(error)
+            }
+        }
+        
+        dispatch_once(&Static.resignFirstResponderToken) {
+            let originalSelector = #selector(UITextView.resignFirstResponder)
+            let swizzledSelector = #selector(UITextView.AM_testBypassResignFirstResponder)
+            do { try UITextView.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
+                debugPrint(error)
+            }
+        }
+        
+    }
+    
+    func AM_testBypassBecomeFirstResponder() -> Bool {
+        shouldHaveMockWindow = true
+        defer { shouldHaveMockWindow = false }
+        return self.AM_testBypassBecomeFirstResponder()
+        
+    }
+    
+    func AM_testBypassResignFirstResponder() -> Bool {
+        shouldHaveMockWindow = true
+        defer { shouldHaveMockWindow = false }
+        return self.AM_testBypassResignFirstResponder()
+    }
+    
+}
+
--- Pod/Classes/UIViewController+TestHelpers.swift
+++ Pod/Classes/UIViewController+TestHelpers.swift
@@ -10,56 +10,97 @@ import UIKit
 import JRSwizzle
 
 extension UIViewController {
-  public override class func initialize() {
-    struct Static {
-      static var presentToken: dispatch_once_t = 0
-      static var dismissToken: dispatch_once_t = 0
+    public override class func initialize() {
+        struct Static {
+            static var presentToken: dispatch_once_t = 0
+            static var dismissToken: dispatch_once_t = 0
+            static var presentedViewControllerToken: dispatch_once_t = 0
+            static var presentingViewControllerToken: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.presentToken) {
+            let originalSelector = Selector("presentViewController:animated:completion:")
+            let swizzledSelector = Selector("AM_testPresentViewController:animated:completion:")
+            
+            do { try UIViewController.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) }
+            catch { debugPrint(error) }
+        }
+        
+        dispatch_once(&Static.dismissToken) {
+            let originalSelector = Selector("dismissViewControllerAnimated:completion:")
+            let swizzledSelector = Selector("AM_testDismissViewControllerAnimated:completion:")
+            
+            do { try UIViewController.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) }
+            catch { debugPrint(error) }
+        }
+        
+        dispatch_once(&Static.presentedViewControllerToken) {
+            let originalSelector = Selector("presentedViewController")
+            let swizzledSelector = Selector("AM_testGetPresentedViewController")
+            
+            do { try UIViewController.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) }
+            catch { debugPrint(error) }
+        }
+        
+        dispatch_once(&Static.presentingViewControllerToken) {
+            let originalSelector = Selector("presentingViewController")
+            let swizzledSelector = Selector("AM_testGetPresentingViewController")
+            
+            do { try UIViewController.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) }
+            catch { debugPrint(error) }
+        }
+        
     }
     
-    dispatch_once(&Static.presentToken) {
-      let originalSelector = Selector("presentViewController:animated:completion:")
-      let swizzledSelector = Selector("AM_testPresentViewController:animated:completion:")
-      
-      do { try UIViewController.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
-        debugPrint(error)
-      }
+    func AM_testPresentViewController(viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)?) {
+        viewControllerToPresent.AM_testPresentingViewController = self
+        self.AM_testPresentedViewController = viewControllerToPresent
+        completion?()
     }
     
-    dispatch_once(&Static.dismissToken) {
-      let originalSelector = Selector("dismissViewControllerAnimated:completion:")
-      let swizzledSelector = Selector("AM_testDismissViewControllerAnimated:completion:")
-      
-      do { try UIViewController.self.jr_swizzleMethod(originalSelector, withMethod: swizzledSelector) } catch {
-        debugPrint(error)
-      }
+    func AM_testDismissViewControllerAnimated(flag: Bool, completion: (() -> Void)?) {
+        self.AM_testPresentedViewController?.AM_testPresentingViewController = nil
+        self.AM_testPresentedViewController = nil
+        completion?()
     }
     
-  }
-  
-  func AM_testPresentViewController(viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)?) {
-    self.AM_testPresentedViewController = viewControllerToPresent
-    completion?()
-  }
-  
-  func AM_testDismissViewControllerAnimated(flag: Bool, completion: (() -> Void)?) {
-    self.AM_testPresentedViewController = nil
-    completion?()
-  }
-  
-  private struct AssociatedKeys {
-    static var TestPresentedViewController = "AM_testPresentedViewController"
-  }
-  
-  public var AM_testPresentedViewController: UIViewController? {
-    get {
-      return objc_getAssociatedObject(self, &AssociatedKeys.TestPresentedViewController) as? UIViewController
+    func AM_testGetPresentedViewController() -> UIViewController? {
+        return AM_testPresentedViewController
     }
-    set {
-      objc_setAssociatedObject(self,
-        &AssociatedKeys.TestPresentedViewController,
-        newValue,
-        objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    
+    func AM_testGetPresentingViewController() -> UIViewController? {
+        return AM_testPresentingViewController
     }
-  }
-  
+
+    // MARK: Associated Objects
+    
+    private struct AssociatedKeys {
+        static var TestPresentedViewController = "AM_testPresentedViewController"
+        static var TestPresentingViewController = "AM_testPresentingViewController"
+    }
+    
+    var AM_testPresentedViewController: UIViewController? {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.TestPresentedViewController) as? UIViewController
+        }
+        set {
+            objc_setAssociatedObject(self,
+                                     &AssociatedKeys.TestPresentedViewController,
+                                     newValue,
+                                     objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    var AM_testPresentingViewController: UIViewController? {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.TestPresentingViewController) as? UIViewController
+        }
+        set {
+            objc_setAssociatedObject(self,
+                                     &AssociatedKeys.TestPresentingViewController,
+                                     newValue,
+                                     objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
 }
